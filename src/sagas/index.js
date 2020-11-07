8a78e2c9bc7e040e498d2cd081e4ab44c30682ec
SumitKPandit <varun1004005@gmail.com>
2020-11-07 21:31:38 +0530

    Implement basic saga

---
+++ src/sagas/index.js
@@ -0,0 +1,16 @@
+import { takeEvery, put } from "redux-saga/effects";
+
+function* workerSaga() {
+    console.log("Hey from worker");
+    console.log(put({ type: "ACTION_FROM_WORKER" }));
+    yield put({ type: "ACTION_FROM_WORKER" });
+}
+
+// watcher saga
+function* rootSaga() {
+    yield takeEvery("HELLO", workerSaga);
+};
+
+// watcher saga -> actions -> worker saga
+
+export default rootSaga;
